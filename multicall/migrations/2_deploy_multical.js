e3926200d9d10fe3450c059b86e11b22b7c47999
Tricker-scnd <igor.stuenko@moonion.com>
2022-08-15 11:45:02 +0300

    remove contracts

--- multicall/migrations/2_deploy_multical.js
+++
@@ -1,6 +0,0 @@
-const UniswapInterfaceMulticall = artifacts.require("UniswapInterfaceMulticall");
-
-module.exports = async function (deployer, network, addresses) {
-
-  await deployer.deploy(UniswapInterfaceMulticall)
-};
